f85ed31aeaa74040c90c3246a92b8446d17ada37
lightninglk55 <148565710+lightninglk55@users.noreply.github.com>
2023-12-08 12:24:47 +0800

    tiktok

---
+++ tiktok/src/App.js
@@ -0,0 +1,14 @@
+import { Routes, Route, Link } from 'react-router-dom'
+
+
+
+function App() {
+  
+  return (
+    <div style={{ padding: 20,}}>
+      
+    </div>
+  );
+}
+
+export default App;
